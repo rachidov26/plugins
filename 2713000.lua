22b75b71c10bfea1a122c30693d010a8ee86b07d
Rachidov32 <94831464+rachidov26@users.noreply.github.com>
2026-09-01 20:00:27 +0100

    Add files via upload

---
+++ 2713000.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2713000 - Resonance: A Plague Tale Legacy
+-- Generated 2026-08-31 16:53:08 UTC
+-- # Depots (Total/DLC/Shared): 3/1/1
+
+-- Main AppID
+addappid(2713000)
+addtoken(2713000, "17223300556103744127")
+
+-- Main Depots
+addappid(2713001, 1, "7085af990ca726e5a5e97ac1acd51bf4f32ba387d0f3d5edb114720e52b536b4")
+setManifestid(2713001, "4228535050004172075", 81435135835)
+
+-- DLC's (no depot keys required)
+addappid(3837640) -- Resonance: A Plague Tale Legacy - Heritage Pack DLC
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 2713002
